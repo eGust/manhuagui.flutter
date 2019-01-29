be3f868c112d02b4d055563dcad780f9b25f7736
Yesterday17 <t@yesterday17.cn>
2019-01-29 17:34:58 +0800

    make configuration scrollable & fix overflow

--- lib/components/home/route_configuration.dart
+++ lib/components/home/route_configuration.dart
@@ -34,120 +34,127 @@ class _RouteConfigurationState extends State<RouteConfiguration> {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           TopBarFrame(),
-          Container(
-            padding: const EdgeInsets.fromLTRB(18.0, 8.0, 0.0, 4.0),
-            decoration: const BoxDecoration(
-              border: Border(bottom: BorderSide(color: Colors.orange)),
-            ),
-            child: const Text('黑名单', style: TextStyle(fontSize: 24.0)),
-          ),
-          Container(
-            padding: const EdgeInsets.all(15.0),
-            decoration: const BoxDecoration(
-              border:
-                  Border(bottom: BorderSide(color: Colors.brown, width: 2.0)),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: globals.metaData.comicFilterGroupList
-                  .map(
-                    (grp) => Wrap(
-                          spacing: 8.0,
-                          runSpacing: -10.0,
-                          children: grp.filters
-                              .map((filter) => _BlacklistButton(
-                                    filter,
-                                    blocked: _isBlocked(filter),
-                                    onPressed: () {
-                                      setState(() {
-                                        if (_isBlocked(filter)) {
-                                          globals.blacklistSet
-                                              .remove(filter.link);
-                                        } else {
-                                          globals.blacklistSet.add(filter.link);
-                                        }
-                                        globals.save();
-                                      });
-                                    },
-                                  ))
-                              .toList(),
-                        ),
-                  )
-                  .toList(),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.fromLTRB(18.0, 4.0, 0.0, 4.0),
-            decoration: const BoxDecoration(
-              border: Border(bottom: BorderSide(color: Colors.orange)),
-            ),
-            child: const Text('杂项', style: TextStyle(fontSize: 24.0)),
-          ),
-          Container(
-            padding: const EdgeInsets.all(5.0),
-            decoration: const BoxDecoration(
-              border:
-                  Border(bottom: BorderSide(color: Colors.brown, width: 2.0)),
-            ),
-            child: Wrap(
-              children: [
-                RaisedButton(
-                  color: Colors.lightBlue[800],
-                  child: const Text('缓存',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16.0,
-                      )),
-                  onPressed: () async {
-                    var cleanning = false;
-                    await showDialog<void>(
-                      context: context,
-                      barrierDismissible: false,
-                      builder: (context) => SimpleDialog(
-                            title: const Text('清除缓存'),
-                            children: [
-                              cleanning
-                                  ? const Text('清除中...')
-                                  : RaisedButton(
-                                      onPressed: () async {
-                                        setState(() {
-                                          cleanning = true;
-                                        });
-                                        await globals.cleanCacheManager();
-                                        setState(() {
-                                          cleanning = false;
-                                        });
-                                      },
-                                      child: const Text('清理'),
-                                    ),
-                              RaisedButton(
-                                onPressed: cleanning
-                                    ? null
-                                    : () {
-                                        Navigator.pop(context);
-                                      },
-                                child: const Text('关闭'),
-                              ),
-                            ],
-                          ),
-                    );
-                  },
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.fromLTRB(18.0, 0.0, 0.0, 4.0),
+                  decoration: const BoxDecoration(
+                    border: Border(bottom: BorderSide(color: Colors.orange)),
+                  ),
+                  child: const Text('黑名单', style: TextStyle(fontSize: 24.0)),
                 ),
-                Container(width: 10.0),
-                RaisedButton(
-                  color: Colors.lightBlue[800],
-                  child: const Text('同步...',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16.0,
-                      )),
-                  onPressed: () {
-                    GoogleDriverSyncManager().syncData();
-                  },
+                Container(
+                  padding: const EdgeInsets.all(15.0),
+                  decoration: const BoxDecoration(
+                    border: Border(
+                        bottom: BorderSide(color: Colors.brown, width: 2.0)),
+                  ),
+                  child: Column(
+                    //crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: globals.metaData.comicFilterGroupList
+                        .map(
+                          (grp) => Wrap(
+                                spacing: 8.0,
+                                runSpacing: -10.0,
+                                children: grp.filters
+                                    .map((filter) => _BlacklistButton(
+                                          filter,
+                                          blocked: _isBlocked(filter),
+                                          onPressed: () {
+                                            setState(() {
+                                              if (_isBlocked(filter)) {
+                                                globals.blacklistSet
+                                                    .remove(filter.link);
+                                              } else {
+                                                globals.blacklistSet
+                                                    .add(filter.link);
+                                              }
+                                              globals.save();
+                                            });
+                                          },
+                                        ))
+                                    .toList(),
+                              ),
+                        )
+                        .toList(),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(18.0, 4.0, 0.0, 4.0),
+                  decoration: const BoxDecoration(
+                    border: Border(bottom: BorderSide(color: Colors.orange)),
+                  ),
+                  child: const Text('杂项', style: TextStyle(fontSize: 24.0)),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(5.0),
+                  decoration: const BoxDecoration(
+                    border: Border(
+                        bottom: BorderSide(color: Colors.brown, width: 2.0)),
+                  ),
+                  child: Wrap(
+                    children: [
+                      RaisedButton(
+                        color: Colors.lightBlue[800],
+                        child: const Text('缓存',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16.0,
+                            )),
+                        onPressed: () async {
+                          var cleanning = false;
+                          await showDialog<void>(
+                            context: context,
+                            barrierDismissible: false,
+                            builder: (context) => SimpleDialog(
+                                  title: const Text('清除缓存'),
+                                  children: [
+                                    cleanning
+                                        ? const Text('清除中...')
+                                        : RaisedButton(
+                                            onPressed: () async {
+                                              setState(() {
+                                                cleanning = true;
+                                              });
+                                              await globals.cleanCacheManager();
+                                              setState(() {
+                                                cleanning = false;
+                                              });
+                                            },
+                                            child: const Text('清理'),
+                                          ),
+                                    RaisedButton(
+                                      onPressed: cleanning
+                                          ? null
+                                          : () {
+                                              Navigator.pop(context);
+                                            },
+                                      child: const Text('关闭'),
+                                    ),
+                                  ],
+                                ),
+                          );
+                        },
+                      ),
+                      Container(width: 10.0),
+                      RaisedButton(
+                        color: Colors.lightBlue[800],
+                        child: const Text('同步...',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16.0,
+                            )),
+                        onPressed: () {
+                          GoogleDriverSyncManager().syncData();
+                        },
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
-          ),
+          )
         ],
       );
 }
